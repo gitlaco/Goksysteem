d6468c86c29bea1a987ccf33bafaf26e199a162b
borjantj <84790368+jorjanc@users.noreply.github.com>
2021-12-03 14:12:20 +0100

    commit

---
+++ Goksysteem/Form2.Designer.cs
@@ -0,0 +1,132 @@
+﻿using System.ComponentModel;
+
+namespace WindowsServiceSchedularApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xz = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xz
+            // 
+            this.xz.Location = new System.Drawing.Point(219, 321);
+            this.xz.Name = "xz";
+            this.xz.Size = new System.Drawing.Size(131, 33);
+            this.xz.TabIndex = 0;
+            this.xz.Text = "Login";
+            this.xz.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(301, 295);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(219, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 163);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Wachtwoord:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.xz);
+            this.Name = "Form1";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button xz;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Goksysteem/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;  
+using System.Collections.Generic;  
+using System.ComponentModel;  
+using System.Data;  
+using System.Data.SqlClient;  
+using System.Drawing;  
+using System.Linq;  
+using System.Text;  
+using System.Threading.Tasks;  
+using System.Windows.Forms;  
+namespace WindowsServiceSchedularApp {  
+    public partial class Form1: Form {  
+        public Form1() {  
+            InitializeComponent();  
+        }  
+        private void label1_Click(object sender, EventArgs e) {}  
+        private void button1_Click(object sender, EventArgs e) {  
+            //Connection String   
+            SqlConnection con = new SqlConnection(@ "Data Source=STL-4110;Initial Catalog=Practice;Integrated Security=True");  
+            SqlCommand cmd = new SqlCommand("select * from Userlogins where UserName=@UserName and Password =@Password", con);  
+            cmd.Parameters.AddWithValue("@UserName", textBox1.Text);  
+            cmd.Parameters.AddWithValue("@Password", textBox2.Text);  
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);  
+            DataTable dt = new DataTable();  
+            sda.Fill(dt);  
+            //Connection open here   
+            con.Open();  
+            int i = cmd.ExecuteNonQuery();  
+            con.Close();  
+            if (dt.Rows.Count > 0) {  
+                MessageBox.Show("Successfully loged in");  
+                //after successful it will redirect  to next page .  
+                WelcomePage settingsForm = new WelcomePage();  
+                settingsForm.Show();  
+            } else {  
+                MessageBox.Show("Please enter Correct Username and Password");  
+            }  
+        }  
+    }  
+}
